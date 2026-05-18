8f0bbdd685dbe19d1a3a0ec9e2b5b4da227e6fef
Riley Wheadon <riley.wheadon@sap.com>
2026-05-17 22:36:16 -0700

    fix: newline in deploy script

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,10 +2,11 @@ set -e -o pipefail
 
 ./scripts/whitespace.sh
 ./scripts/compile.sh
+echo ""
 
 jj bookmark set master > /dev/null 2>&1
 jj git push --remote=origin > /dev/null 2>&1
 
-echo "\nBuilding and pushing Docker image..."
+echo "Building and pushing Docker image..."
 docker build -t rileywheadon/personal-website:latest .
 docker push rileywheadon/personal-website:latest
